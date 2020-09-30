b8b5a788d57470fe4a15b6ee85fcf907b49d76ed
SSung <50323157+Sung2019@users.noreply.github.com>
2020-10-01 00:01:42 +0900

    기본 Scene 구성
    Level selection, Leve 01~13 Scene 구성

---
+++ Level-system/Assets/Scripts/LevelSelection.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelSelection : MonoBehaviour
+{
+
+    [SerializeField] private bool unlocked; // Default value == false
+
+    public Image unlockImage;
+    public GameObject[] stars;
+
+
+    private void UpdateLevelImage()
+    {
+        if (!unlocked) //MARKER if unclock is false means This level is clocked!
+        {
+            
+        }
+    }
+
+}
