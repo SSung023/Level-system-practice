5bfe418b3ab0ce8d9e4ae56622a70adde1e7443f
SSung <50323157+Sung2019@users.noreply.github.com>
2020-10-11 20:01:43 +0900

    [Project] LevelSelecion Image change
    Level Selection Scene에서 unlock 상태 변화 시, 이미지 변환

--- Level-system/Assets/Scripts/LevelSelection.cs
+++ Level-system/Assets/Scripts/LevelSelection.cs
@@ -1,18 +1,57 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelSelection : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private bool isUnlocked; //Default 값 == false
+
+    public Image unlockImage;
+    public GameObject[] stars;
+
+
+
+    private void Update()
     {
+        UpdateLevelImage();
+    }
+    
+    private void UpdateLevelImage()
+    {
+        //만일 isUnlocked 비활성화라면 자물쇠 이미지 Set true
+        if (!isUnlocked)
+        {
+            unlockImage.gameObject.SetActive(true);
+            for (int i = 0; i < stars.Length; i++)
+            {
+                stars[i].SetActive(false);
+            }
+        }
         
+        // level이 unlock됐다면 이미지 change
+        else
+        {
+            unlockImage.gameObject.SetActive(false);
+            for (int i = 0; i < stars.Length; i++)
+            {
+                stars[i].SetActive(true);
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void PressSelection(string _LevelName) //버튼을 눌렀을 때, 해당 Scene으로 이동하게 하는 Method
     {
-        
+        if (isUnlocked)
+        {
+            SceneManager.LoadScene(_LevelName);
+        }
     }
+    
+    
+
+
 }
